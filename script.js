5c1e001de71c432bc07c8aac00de6049d8d1b6a8
unkuninho <139827219+unkuninho@users.noreply.github.com>
2024-03-29 12:37:01 -0300

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function redirectToTwitter(partida) {
-    var mensagem = "Acabou de assistir a partida " + partida;
-    var imagem = "https://example.com/" + partida + ".jpg"; // Substitua example.com pelo local real da imagem
+function redirectToTwitter(partida, imagem) {
+  var mensagem = "Acabou de assistir a partida " + partida;
+  var imagemUrl = window.location.href + imagem;
+
+  var tweetUrl = "https://twitter.com/compose/tweet?text=" + encodeURIComponent(mensagem) + "&url=" + encodeURIComponent(imagemUrl);
   
-    var tweetUrl = "https://twitter.com/compose/tweet?text=" + encodeURIComponent(mensagem) + "&url=" + encodeURIComponent(imagem);
-    
-    window.open(tweetUrl, "_blank");
-  }
-  
+  window.open(tweetUrl, "_blank");
+}
