dfdf1250b1c65a9ea1764f564ba7c307856373e9
unkuninho <139827219+unkuninho@users.noreply.github.com>
2024-03-29 12:50:26 -0300

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function redirectToTwitter(partida) {
   var mensagem = "Acabou de assistir a partida " + partida;
+  var imagemUrl = "https://raw.githubusercontent.com/unkuninho/futeboltracker/main/fcbarcelonaxrealmadrid.jpg";
 
-  // Abra a página de composição do tweet com a mensagem
-  var tweetUrl = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(mensagem);
+  // Abra a página de composição do tweet com a mensagem e a URL da imagem
+  var tweetUrl = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(mensagem) + "&url=" + encodeURIComponent(imagemUrl);
   
   window.open(tweetUrl, "_blank");
 }
