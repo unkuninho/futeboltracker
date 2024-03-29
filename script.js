fa72f4477f3f7a5f6f5c579083b78f407671c90d
unkuninho <139827219+unkuninho@users.noreply.github.com>
2024-03-29 12:39:21 -0300

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function redirectToTwitter(partida, imagem) {
   var mensagem = "Acabou de assistir a partida " + partida;
   var imagemUrl = window.location.href + imagem;
+  var tweetText = mensagem + " " + imagemUrl;
 
-  var tweetUrl = "https://twitter.com/compose/tweet?text=" + encodeURIComponent(mensagem) + "&url=" + encodeURIComponent(imagemUrl);
+  var tweetUrl = "https://twitter.com/compose/tweet?text=" + encodeURIComponent(tweetText);
   
   window.open(tweetUrl, "_blank");
 }
